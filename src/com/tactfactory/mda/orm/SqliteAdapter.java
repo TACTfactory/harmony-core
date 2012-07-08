36c588777ebe94d8b1a3cad7f76c236427b19994
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2012-07-08 03:31:03 +0200

    Clean project

--- src/com/tactfactory/mda/orm/SqliteAdapter.java
+++ src/com/tactfactory/mda/orm/SqliteAdapter.java
@@ -1,6 +1,12 @@
+/**
+ * This file is part of the Harmony package.
+ *
+ * (c) Mickael Gaillard <mickael.gaillard@tactfactory.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 package com.tactfactory.mda.orm;
-
-import com.tactfactory.mda.orm.annotation.Column;
 
 public class SqliteAdapter {
 	private static String PREFIX = "COL_";
