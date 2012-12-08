c6f347a47c240190ac7b80b2e8f237dc7ab51c2a
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2012-12-08 01:29:42 +0100

    Add application template

---
+++ tpl/android/src/TemplateApplication.java
@@ -0,0 +1,11 @@
+package ${project_namespace};
+
+import ${project_namespace}.R;
+
+/** Custom ${project_name} Application context */
+public class ${project_name}Application extends ApplicationBase {
+	// Override or Create your custom method for your application
+	// this file is just generate at first time, and never override...
+	
+	
+}
