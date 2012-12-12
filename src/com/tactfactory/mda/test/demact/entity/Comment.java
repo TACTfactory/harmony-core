d6767f59155b28d26e15004b858af092d011e636
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2012-12-12 21:26:48 +0100

    Refactor Test

--- src/com/tactfactory/mda/test/demact/entity/Comment.java
+++ src/com/tactfactory/mda/test/demact/entity/Comment.java
@@ -8,22 +8,22 @@
  */
 package com.tactfactory.mda.test.demact.entity;
 
-import java.io.Serializable;
 import java.util.Date;
-
 import org.joda.time.DateTime;
 
 import com.tactfactory.mda.orm.annotation.*;
+import com.tactfactory.mda.orm.annotation.Column.Type;
 
 @Table
 @Entity
-public class Comment implements Serializable {
+public class Comment extends Object {
+	
 	@Id
-    @Column()					// type="integer",
+    @Column()
     @GeneratedValue(strategy="IDENTITY")
     protected int id;
 
-	@Column(length=2000)	// type="string"
+	@Column(length=2000, type=Type.TEXT)
     protected String content;
 	
 	@ManyToOne
@@ -34,79 +34,13 @@ public class Comment implements Serializable {
 	
 	@Column(name="created_at")	// type="datetime",
     protected Date createdAt;
-
+	
+	@Column
+	protected boolean validate = false;
 
 	public Comment() {
 		this.id = -1;
     	this.createdAt = new DateTime().toDate();
     }
-	/**
-	 * @return the id
-	 */
-	public final int getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public final void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the content
-	 */
-	public final String getContent() {
-		return content;
-	}
-
-	/**
-	 * @param content the content to set
-	 */
-	public final void setContent(String content) {
-		this.content = content;
-	}
-
-	/**
-	 * @return the owner
-	 */
-	public final User getOwner() {
-		return owner;
-	}
-
-	/**
-	 * @param owner the owner to set
-	 */
-	public final void setOwner(User owner) {
-		this.owner = owner;
-	}
-
-	/**
-	 * @return the post
-	 */
-	public final Post getPost() {
-		return post;
-	}
-
-	/**
-	 * @param post the post to set
-	 */
-	public final void setPost(Post post) {
-		this.post = post;
-	}
-
-	/**
-	 * @return the createdAt
-	 */
-	public final Date getCreatedAt() {
-		return createdAt;
-	}
-
-	/**
-	 * @param createdAt the createdAt to set
-	 */
-	public final void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
+	
 }
