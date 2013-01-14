91d9b3eb7019af394b92cb1ab0f9285dc795810c
Gregg Cesarine <gregg.cesarine@tactfactory.com>
2013-01-14 16:50:28 +0100

    Fix #1273

---
+++ src/com/tactfactory/mda/sync/test/AllTests.java
@@ -0,0 +1,17 @@
+package com.tactfactory.mda.sync.test;
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	SyncGlobalTest.class
+		})
+                
+public class AllTests {
+        /*public static void main(String[] args) {
+                JUnitCore.runClasses(new Class[] { AllTests.class });
+        }*/
+}
