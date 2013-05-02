2d76e62eab8c9b7e0f4b5c580252aacbf290693b
Gregg Cesarine <gregg.cesarine@tactfactory.com>
2013-05-02 18:28:11 +0200

    Fix #2147

--- src/com/tactfactory/mda/plateforme/SqliteAdapter.java
+++ src/com/tactfactory/mda/plateforme/SqliteAdapter.java
@@ -36,7 +36,7 @@ public abstract class SqliteAdapter {
 		builder.append(generateColumnType(fm.getColumnDefinition()));
 		if (fm.isId()) {
 			builder.append(" PRIMARY KEY");
-			if (fm.getColumnDefinition().equals("integer")) {
+			if (fm.getColumnDefinition().equals("INTEGER")) {
 				builder.append(" AUTOINCREMENT");
 			}
 		} else {
@@ -84,19 +84,42 @@ public abstract class SqliteAdapter {
 	 */
 	public static String generateColumnType(final String fieldType) {
 		String type = fieldType;
-		if (type.equals(Column.Type.STRING.getValue()) 
-			|| type.equals(Column.Type.TEXT.getValue()) 
-			|| type.equals(Column.Type.LOGIN.getValue())
-			|| type.equals(Column.Type.PHONE.getValue())) {
+		if (type.equalsIgnoreCase(Column.Type.STRING.getValue()) 
+			|| type.equalsIgnoreCase(Column.Type.TEXT.getValue()) 
+			|| type.equalsIgnoreCase(Column.Type.LOGIN.getValue())
+			|| type.equalsIgnoreCase(Column.Type.PHONE.getValue())
+			|| type.equalsIgnoreCase(Column.Type.PASSWORD.getValue())
+			|| type.equalsIgnoreCase(Column.Type.EMAIL.getValue())
+			|| type.equalsIgnoreCase(Column.Type.CITY.getValue())
+			|| type.equalsIgnoreCase(Column.Type.ZIPCODE.getValue())
+			|| type.equalsIgnoreCase(Column.Type.COUNTRY.getValue())) {
 			type = "VARCHAR";
 		} else
 			
-		if (type.equals(Column.Type.PASSWORD.getValue())) {
-			type = "VARCHAR";
+		if (type.equalsIgnoreCase(Column.Type.DATETIME.getValue())) {
+			type = "DATETIME";
 		} else
 			
-		if (type.equals(Column.Type.DATETIME.getValue())) {
-			type = "VARCHAR";
+		if (type.equalsIgnoreCase(Column.Type.DATE.getValue())) {
+			type = "DATE";
+		} else
+			
+		if (type.equalsIgnoreCase(Column.Type.TIME.getValue())) {
+			type = "DATETIME";
+		} else
+			
+		if (type.equalsIgnoreCase(Column.Type.BOOLEAN.getValue())) {
+			type = "BOOLEAN";
+		} else
+			
+		if (type.equalsIgnoreCase(Column.Type.INTEGER.getValue())
+				|| type.equalsIgnoreCase(Column.Type.INT.getValue())
+				|| type.equalsIgnoreCase(Column.Type.BC_EAN.getValue())) {
+			type = "INTEGER";
+		} else
+		
+		if (type.equalsIgnoreCase(Column.Type.FLOAT.getValue())) {
+			type = "FLOAT";
 		}
 
 		return type;
@@ -127,7 +150,7 @@ public abstract class SqliteAdapter {
 	 */
 	public static String generateColumnDefinition(final String type) {
 		String ret = type;
-		if (type.equals("int")) {
+		if (type.equalsIgnoreCase("int")) {
 			ret = "integer";
 		}
 		return ret;
