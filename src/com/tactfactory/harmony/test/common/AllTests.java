42cf17179ee45f011dba6d72edd6e410c52058e6
Gregg Cesarine <gregg.cesarine@tactfactory.com>
2014-03-19 17:27:09 +0100

    Close #3867

---
+++ src/com/tactfactory/harmony/test/common/AllTests.java
@@ -0,0 +1,24 @@
+/**
+ * This file is part of the Harmony package.
+ *
+ * (c) Mickael Gaillard <mickael.gaillard@tactfactory.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+package com.tactfactory.harmony.test.common;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * All tests related to ORM initialization and generation.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	GenerateStaticViewTest.class})
+
+public class AllTests {
+	
+}
---
+++ tpl/android/res/layout/activity_template_static.xml.ftl
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<FrameLayout
+	xmlns:android="http://schemas.android.com/apk/res/android"
+	xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+
+    <fragment
+        android:id="@+id/fragment"
+        android:name="${project_namespace}.view.${viewPackage}.${viewName}Fragment"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        tools:layout="@layout/fragment_${viewName?lower_case}" >
+    </fragment>
+
+</FrameLayout>
